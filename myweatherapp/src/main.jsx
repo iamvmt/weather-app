0abc08cde489f9bc78bc9e7b510e5eb039bd5f6b
Vivek Mani Tripathy <vmtprimary@gmail.com>
2025-08-14 04:07:16 +0530

    Strict Mode enabled in main.jsx

--- myweatherapp/src/main.jsx
+++ myweatherapp/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
